bdde80d424864b39abd56c5f2e6e912cc8b8c305
2xmax <2xmax@mail.ru>
2013-09-14 15:14:03 -0700

    sample website

---
+++ demo/System.Web.StaticOptimization.Samples/SimpleMvcWebsite/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SimpleMvcWebsite
+{
+    using System.Web.StaticOptimization.Mvc;
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+#if DEBUG
+            BundleTable.Init(System.Web.HttpContext.Current.Server.MapPath("~/bundles.config"));
+#endif
+        }
+    }
+}
