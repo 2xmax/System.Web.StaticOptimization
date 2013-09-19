5f1960a1f12e720acaa77a5ad3d5d0e558bdcb6f
2xmax <2xmax@mail.ru>
2013-09-18 18:08:22 -0700

    simple spa demo

---
+++ demo/SimpleDurandalSpa/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleDurandalSpa.MvcApplication" Language="C#" %>
---
+++ demo/SimpleDurandalSpa/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace SimpleDurandalSpa
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+              name: "DefaultApi",
+              routeTemplate: "api/{controller}/{id}",
+              defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
